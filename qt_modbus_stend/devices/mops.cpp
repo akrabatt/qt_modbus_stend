b5c903a0bc21b00241d0a91d9f8034da6b13b0a9
akrabatt <starwars-1999@yandex.ru>
2024-09-05 10:26:11 +0200

    class mops ready

--- qt_modbus_stend/devices/mops.cpp
+++ qt_modbus_stend/devices/mops.cpp
@@ -1,6 +1,20 @@
 #include "mops.h"
 
-mops::mops()
-{
+// пустой конструктор по умолчанию
+mops::mops(){}
 
+/**
+ * @brief mops::mops параметризированный конструктор мопса
+ * @param id адресс модуля
+ */
+mops::mops(const int id) : id(id) {}
+
+
+/**
+ * @brief mops::mops_set_operable_status устанавливает статус исправности
+ * @param status передаем сам статус
+ */
+void mops::mops_set_operable_status(bool status)
+{
+    this -> operable = status;  // ставим сам статус
 }
--- qt_modbus_stend/devices/mops.h
+++ qt_modbus_stend/devices/mops.h
@@ -1,46 +1,63 @@
 #ifndef MOPS_H
 #define MOPS_H
 
-#include <cstdint>
 
 class mops
 {
 private:
-    int id;
-    bool operable;
+    int id;         // mupses id
+    bool operable;  // operable status
+
+    // структура данных состояния мопса
     union tag_mops_stand_statment
     {
         struct
         {
             struct
             {
-                uint16_t mops_online;                 // is there connection with module
-                uint16_t mops_online_err;             // connection error
-                unsigned short mops_offline;                // mops not connected special
-                unsigned short mops_operable;               // is the module working properly or not
-                unsigned short mops_not_operable;           // the module is not working properly
+                int mops_online;                 // is there connection with module
+                int mops_online_err;             // connection error
+                int mops_offline;                // mops not connected special
+                int mops_operable;               // is the module working properly or not
+                int mops_not_operable;           // the module is not working properly
             }mops_statment;
-            unsigned short mops_current_ch_status[8];                   // buffer for the value of the states
+            int mops_current_ch_status[8];                   // buffer for the value of the states
             struct
             {
-                unsigned short mops_ch_err_break[8];                   // break(1)
-                unsigned short mops_ch_err_normal[8];                  // normal(2)
-                unsigned short mops_ch_err_attantion[8];               // attantion(4)
-                unsigned short mops_ch_err_fire[8];                    // fire(5)
-                unsigned short mops_ch_err_sc[8];                      // short current(6)
+                int mops_ch_err_break[8];                   // break(1)
+                int mops_ch_err_normal[8];                  // normal(2)
+                int mops_ch_err_attantion[8];               // attantion(4)
+                int mops_ch_err_fire[8];                    // fire(5)
+                int mops_ch_err_sc[8];                      // short current(6)
             }mops_ch_statement;
             struct
             {
-                unsigned short mops_18v_error;
-                unsigned short mops_24v_error;
-                unsigned short mops_28v_error;
+                int mops_18v_error;
+                int mops_24v_error;
+                int mops_28v_error;
             }mops_power_supply_error;
         };
-        unsigned short main_buff[56];
+        int main_buff[56];
     }mops_stand_statment;
 
 public:
+    // пустой конструктор
     mops();
+
+    /**
+     * @brief mops параметризированный конструктор
+     * @param id адрес мупса
+     */
+    mops(const int id);
+
+    // деструктор
+    ~mops();
+
+    /**
+     * @brief mops_set_operable_status устанавливает статус исправен или нет
+     * @param status сам статус
+     */
+    void mops_set_operable_status(bool status);
 };
 
 #endif // MOPS_H
