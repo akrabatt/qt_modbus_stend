0b3087dcb0550f5590b243cb6c996da82dc5f8a9
akrabatt <starwars-1999@yandex.ru>
2024-09-06 15:22:20 +0200

    save point

--- build-qt_modbus_stend-Desktop_Qt_6_2_4_MSVC2019_64bit-Debug/ui_stend_main_window.h
+++ build-qt_modbus_stend-Desktop_Qt_6_2_4_MSVC2019_64bit-Debug/ui_stend_main_window.h
@@ -11,6 +11,7 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
@@ -38,6 +39,32 @@ public:
     QSpacerItem *horizontalSpacer_2;
     QPushButton *button_test_connection;
     QLabel *answer_connection_lable;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout_2;
+    QVBoxLayout *verticalLayout_3;
+    QCheckBox *checkBox_active_mops_1;
+    QCheckBox *checkBox_active_mops_2;
+    QCheckBox *checkBox_active_mops_3;
+    QCheckBox *checkBox_active_mops_4;
+    QCheckBox *checkBox_active_mops_5;
+    QCheckBox *checkBox_active_mops_6;
+    QCheckBox *checkBox_active_mops_7;
+    QCheckBox *checkBox_active_mops_8;
+    QCheckBox *checkBox_active_mops_9;
+    QCheckBox *checkBox_active_mops_10;
+    QSpacerItem *horizontalSpacer_3;
+    QVBoxLayout *verticalLayout_2;
+    QCheckBox *checkBox_active_mups_1;
+    QCheckBox *checkBox_active_mups_2;
+    QCheckBox *checkBox_active_mups_3;
+    QCheckBox *checkBox_active_mups_4;
+    QCheckBox *checkBox_active_mups_5;
+    QCheckBox *checkBox_active_mups_6;
+    QCheckBox *checkBox_active_mups_7;
+    QCheckBox *checkBox_active_mups_8;
+    QCheckBox *checkBox_active_mups_9;
+    QCheckBox *checkBox_active_mups_10;
+    QLabel *label;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -45,12 +72,12 @@ public:
     {
         if (stend_main_window->objectName().isEmpty())
             stend_main_window->setObjectName(QString::fromUtf8("stend_main_window"));
-        stend_main_window->resize(1920, 1026);
+        stend_main_window->resize(590, 921);
         centralwidget = new QWidget(stend_main_window);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         verticalLayoutWidget = new QWidget(centralwidget);
         verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
-        verticalLayoutWidget->setGeometry(QRect(10, 10, 293, 76));
+        verticalLayoutWidget->setGeometry(QRect(10, 10, 341, 76));
         verticalLayout = new QVBoxLayout(verticalLayoutWidget);
         verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
         verticalLayout->setContentsMargins(0, 0, 0, 0);
@@ -89,11 +116,136 @@ public:
 
         answer_connection_lable = new QLabel(centralwidget);
         answer_connection_lable->setObjectName(QString::fromUtf8("answer_connection_lable"));
-        answer_connection_lable->setGeometry(QRect(320, 60, 121, 31));
+        answer_connection_lable->setGeometry(QRect(360, 60, 121, 31));
+        horizontalLayoutWidget = new QWidget(centralwidget);
+        horizontalLayoutWidget->setObjectName(QString::fromUtf8("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(10, 160, 291, 621));
+        horizontalLayout_2 = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalLayout_3->setContentsMargins(10, 0, 10, -1);
+        checkBox_active_mops_1 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_1->setObjectName(QString::fromUtf8("checkBox_active_mops_1"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_1);
+
+        checkBox_active_mops_2 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_2->setObjectName(QString::fromUtf8("checkBox_active_mops_2"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_2);
+
+        checkBox_active_mops_3 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_3->setObjectName(QString::fromUtf8("checkBox_active_mops_3"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_3);
+
+        checkBox_active_mops_4 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_4->setObjectName(QString::fromUtf8("checkBox_active_mops_4"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_4);
+
+        checkBox_active_mops_5 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_5->setObjectName(QString::fromUtf8("checkBox_active_mops_5"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_5);
+
+        checkBox_active_mops_6 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_6->setObjectName(QString::fromUtf8("checkBox_active_mops_6"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_6);
+
+        checkBox_active_mops_7 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_7->setObjectName(QString::fromUtf8("checkBox_active_mops_7"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_7);
+
+        checkBox_active_mops_8 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_8->setObjectName(QString::fromUtf8("checkBox_active_mops_8"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_8);
+
+        checkBox_active_mops_9 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_9->setObjectName(QString::fromUtf8("checkBox_active_mops_9"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_9);
+
+        checkBox_active_mops_10 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mops_10->setObjectName(QString::fromUtf8("checkBox_active_mops_10"));
+
+        verticalLayout_3->addWidget(checkBox_active_mops_10);
+
+
+        horizontalLayout_2->addLayout(verticalLayout_3);
+
+        horizontalSpacer_3 = new QSpacerItem(1, 1, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_2->addItem(horizontalSpacer_3);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(10, -1, 10, -1);
+        checkBox_active_mups_1 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_1->setObjectName(QString::fromUtf8("checkBox_active_mups_1"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_1);
+
+        checkBox_active_mups_2 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_2->setObjectName(QString::fromUtf8("checkBox_active_mups_2"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_2);
+
+        checkBox_active_mups_3 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_3->setObjectName(QString::fromUtf8("checkBox_active_mups_3"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_3);
+
+        checkBox_active_mups_4 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_4->setObjectName(QString::fromUtf8("checkBox_active_mups_4"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_4);
+
+        checkBox_active_mups_5 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_5->setObjectName(QString::fromUtf8("checkBox_active_mups_5"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_5);
+
+        checkBox_active_mups_6 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_6->setObjectName(QString::fromUtf8("checkBox_active_mups_6"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_6);
+
+        checkBox_active_mups_7 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_7->setObjectName(QString::fromUtf8("checkBox_active_mups_7"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_7);
+
+        checkBox_active_mups_8 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_8->setObjectName(QString::fromUtf8("checkBox_active_mups_8"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_8);
+
+        checkBox_active_mups_9 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_9->setObjectName(QString::fromUtf8("checkBox_active_mups_9"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_9);
+
+        checkBox_active_mups_10 = new QCheckBox(horizontalLayoutWidget);
+        checkBox_active_mups_10->setObjectName(QString::fromUtf8("checkBox_active_mups_10"));
+
+        verticalLayout_2->addWidget(checkBox_active_mups_10);
+
+
+        horizontalLayout_2->addLayout(verticalLayout_2);
+
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(60, 130, 211, 16));
         stend_main_window->setCentralWidget(centralwidget);
         menubar = new QMenuBar(stend_main_window);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 1920, 25));
+        menubar->setGeometry(QRect(0, 0, 590, 25));
         stend_main_window->setMenuBar(menubar);
         statusbar = new QStatusBar(stend_main_window);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -110,8 +262,29 @@ public:
         just_lable_1->setText(QCoreApplication::translate("stend_main_window", "            \320\242\320\265\321\201\321\202 \321\201\320\276\320\265\320\264\320\270\320\275\320\265\320\275\320\270\321\217 (\321\201\320\272\320\276\321\200\320\276\321\201\321\202\321\214 115200)", nullptr));
         just_lable_2->setText(QCoreApplication::translate("stend_main_window", "COM \320\277\320\276\321\200\321\202:", nullptr));
         line_in_com_num->setPlaceholderText(QCoreApplication::translate("stend_main_window", "COM1...", nullptr));
-        button_test_connection->setText(QCoreApplication::translate("stend_main_window", "\321\202\320\265\321\201\321\202", nullptr));
+        button_test_connection->setText(QCoreApplication::translate("stend_main_window", "\321\202\320\265\321\201\321\202 \321\201\320\276\320\265\320\264\320\270\320\275\320\265\320\275\320\270\321\217", nullptr));
         answer_connection_lable->setText(QString());
+        checkBox_active_mops_1->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 1", nullptr));
+        checkBox_active_mops_2->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 2", nullptr));
+        checkBox_active_mops_3->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 3", nullptr));
+        checkBox_active_mops_4->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 4", nullptr));
+        checkBox_active_mops_5->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 5", nullptr));
+        checkBox_active_mops_6->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 6", nullptr));
+        checkBox_active_mops_7->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 7", nullptr));
+        checkBox_active_mops_8->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 8", nullptr));
+        checkBox_active_mops_9->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 9", nullptr));
+        checkBox_active_mops_10->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\236\320\237\320\241 10", nullptr));
+        checkBox_active_mups_1->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 1", nullptr));
+        checkBox_active_mups_2->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 2", nullptr));
+        checkBox_active_mups_3->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 3", nullptr));
+        checkBox_active_mups_4->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 4", nullptr));
+        checkBox_active_mups_5->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 5", nullptr));
+        checkBox_active_mups_6->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 6", nullptr));
+        checkBox_active_mups_7->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 7", nullptr));
+        checkBox_active_mups_8->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 8", nullptr));
+        checkBox_active_mups_9->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 9", nullptr));
+        checkBox_active_mups_10->setText(QCoreApplication::translate("stend_main_window", "\320\234\320\243\320\237\320\241 10", nullptr));
+        label->setText(QCoreApplication::translate("stend_main_window", "\320\222\321\213\320\261\320\265\321\200\320\270\321\202\320\265 \320\270\321\201\320\277\321\213\321\202\321\203\320\265\320\274\321\213\320\265 \320\274\320\276\320\264\321\203\320\273\320\270", nullptr));
     } // retranslateUi
 
 };
